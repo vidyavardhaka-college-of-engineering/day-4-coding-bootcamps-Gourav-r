41d71711a3cf9429a5d66b5bbb4be6f41c006f7e
Gourav R <20cse0081@vvce.ac.in>
2021-02-18 12:27:26 +0000

    Take the size and elements of array and printing sum as output

--- Program_3.c
+++ Program_3.c
@@ -10,8 +10,17 @@
 int main()
 {
   //Declare all the variables[row, col, array a(2D array), i] used in the code
+  int row, col, array[M][N],i ;
    //Read the Value of row and coloumn of the matrix (i.e size of matrix) using scanf statement
+   scanf("%d",M);
+   scanf("%d",N); 
    //Read the elements of matrix a using nested for loop and scanf statment
+   for(i=0;i<M;i++){
+     scanf("%d",&array[M])
+     for(i=0;M<N:i++){
+       scanf("%d",&array[N])
+     }
    //Print the elements of matrix a using nested for loop and printf statment as per the above specified format
+  
   return 0;
 }
